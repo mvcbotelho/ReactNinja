e64fb96cc59aa7fb3fb946e80081db7db802ca10
mvcbotelho <marcus.itec@gmail.com>
2018-03-04 11:17:11 -0300

    Modify components for adapting styled format

--- src/title.js
+++ src/title.js
@@ -2,9 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 
 const WrapperTitle = styled.h1`
-font-size: 1.5em;
-text-align: center;
-color: palevioletred;
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
 `
 
 const Title = (props) => (
