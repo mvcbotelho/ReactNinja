405ba29dbeaa0af95345794ed97961c174505780
mvcbotelho <marcus.itec@gmail.com>
2018-03-03 23:54:03 -0300

    Add Styled Components in project for study

--- src/title.js
+++ src/title.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Title = (props) => (
-  <h1>Olá {`${props.name} ${props.lastName}`} !!!</h1>
+  <p>Olá {`${props.name} ${props.lastName}`} !!!</p>
 )
 
 Title.defaultProps = {
