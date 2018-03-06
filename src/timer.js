08bdeee64f6a738e3ecb1edee5b5c43d35cb85e6
mvcbotelho <marcus.itec@gmail.com>
2018-03-06 20:24:12 -0300

    challange life cycle

--- src/timer.js
+++ src/timer.js
@@ -14,6 +14,11 @@ class Timer extends Component {
     console.log('componentWillReceiveProps', this.props, nextProps)
   }
 
+  shouldComponentUpdate (nextProps, nextState) {
+    console.log('shouldComponentUpdate', this.props, nextState)
+    return true
+  }
+
   componentDidMount () {
     this.timer = setInterval(() => this.setState({ time: this.state.time + 1 }), 1000)
   }
