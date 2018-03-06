6b6a9b7e7014c977b204f0b7853295d18edfd1a0
mvcbotelho <marcus.itec@gmail.com>
2018-03-06 19:02:45 -0300

    Challange - statefull vs statless

--- src/likeButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Button from './button'
-
-const LikeButton = props => (
-  <Button handleClick={() => alert('Curtir!')}>Curtir</Button>
-)
-
-export default LikeButton
--- src/searchButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Button from './button'
-
-const SearchButton = props => (
-  <Button handleClick={() => alert('Buscar!!!')}>Buscar</Button>
-)
-
-export default SearchButton
